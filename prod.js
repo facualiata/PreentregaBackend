6c629dd450726389c8199b68f28809f5313aeba0
facualiata <facualiata@gmail.com>
2023-06-20 21:32:31 -0300

    inicie el repo

---
+++ prod.js
@@ -0,0 +1,14 @@
+class TicketManager {
+    constructor (nombre){
+        this.nombre = nombre;
+        this.valor = valor;
+    }
+        static eventos = [0];
+
+        incrementar() {
+            this.valor++;
+        }
+
+
+    
+}
