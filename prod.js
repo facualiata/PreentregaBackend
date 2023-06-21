82c39611569a419a352512108b1895387776bf63
facualiata <facualiata@gmail.com>
2023-06-21 13:23:12 -0300

    agregue contructor con funciones de getProducts y getProductsById

--- prod.js
+++ prod.js
@@ -1,14 +1,62 @@
 class TicketManager {
-    constructor (nombre){
-        this.nombre = nombre;
-        this.valor = valor;
+    products;
+
+    constructor (products){
+        this.products = [];
     }
-        static eventos = [0];
-
-        incrementar() {
-            this.valor++;
-        }
-
-
     
+    static correlativoId = 0;
+
+    addProduct(title, description, price, thumbnail, code, stock){
+        
+        if(title == undefined || description== undefined || price == undefined || thumbnail == undefined || code == undefined|| stock == undefined){
+            throw new Error( "todos los campos son obligatorios")
+        
+        }
+        let codeExists = this.products.some((dato) => dato.code == code);
+         if(codeExists){
+            throw  new Error("el codigo ya existe")
+        }else{
+            TicketManager.correlativoId++;
+            const newProduct = {
+                id: TicketManager.correlativoId,
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock
+            };
+            this.products.push(newProduct);
+        }   
+    }
+    getProducts(){
+        return this.products;
+    }
+
+    getProductById(id){
+        let product = this.products.find(dato => dato.id === id);
+
+        if(product !== undefined){
+            return product;
+        }else{
+            return "no existe el producto solicitado";
+        }
+    }
+
 }
+
+ myFirtsProduct = new TicketManager();
+
+myFirtsProduct.addProduct(
+    "producto de prueba",
+    "este es un producto de prueba",
+    200,
+    "sin imagen",
+    "abc123",
+    25,
+);
+
+
+console.log("desde getProducts", myFirtsProduct.getProducts());
+console.log("desde getProductById", myFirtsProduct.getProductById(1));
